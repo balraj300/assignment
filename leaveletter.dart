e7f76cf13a2278b6977c0a86490b7663a318c709
balraj300 <71820497+balraj300@users.noreply.github.com>
2020-09-24 18:42:20 +0530

    Add files via upload

---
+++ leaveletter.dart
@@ -0,0 +1,20 @@
+void main() {
+  print("\t \t \t LEAVE LETTER");
+  print("\t \t \t ************");
+  print("\n From :  J.bhalraj");
+  print("\t 10th std 'B'sec"  );
+  print("\t Marai Malai Adigal Govt High School");
+  print("\t Embalam");
+  print("\n To : \t The Principal" );
+  print("\t Marai Malai Adigal Govt High School");
+  print("\t Embalam");
+  print("\n \t  Sub : Suffering From fever " );
+  
+   print(" Hi Sir,");
+   print("\t  Sufferring from fever************************");
+  print("\t \t \t Thanking You, ");
+print("\t \t \t \t \t Yours Faithfully");
+  print("\t \t \t \t \t ***********");
+  print("date :");
+  print ("place :");
+}
